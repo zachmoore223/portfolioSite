66cf336d3e89d256e9a54db6da4ab98c4d04855e
Zach Moore <sluggermoore@gmail.com>
2023-05-09 19:55:59 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -14,14 +14,13 @@ function App() {
         <h2 id="about-me">About Me</h2>
         <p>dalgjla</p>
         
-          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br />
 
         <h2 id="skills">Skills</h2>
         <p>dalgjla</p>
 
-        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        <br />
+
 
         <h2 id="projects">Projects</h2>
         <p>dalgjla</p>
