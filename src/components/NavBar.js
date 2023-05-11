e75476190049eb183bef054f493fad0b8d4ba090
Zach Moore <sluggermoore@gmail.com>
2023-05-11 13:10:07 -0400

    email added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import github from '../images/github.png'
 import linkedIn from '../images/linkedIn.png'
+import email from '../images/email.png'
 
 export default function NavBar() {
   const [activeLink, setActiveLink] = useState('About Me');
@@ -19,8 +20,11 @@ export default function NavBar() {
       <li className='phone'>
        <p>419-612-7542</p> 
       </li>
-      <li className='email'>
-      <a href="mailto:zachmoore223@gmail.com">zachmoore223@gmail.com</a>
+      <li className='social-links'>
+      <a href="mailto:zachmoore223@gmail.com" target="_blank" rel="noopener noreferrer"><p>E-mail</p></a>
+        <a href="mailto:zachmoore223@gmail.com" target="_blank" rel="noopener noreferrer">
+        <img id='github' src={email} alt="linkedIn logo" height="35px" width="35px"></img>
+        </a>
       </li>
       <li className='social-links'>
       <a href="http://www.linkedin.com/in/zachmoore223" target="_blank" rel="noopener noreferrer"><p>LinkedIn</p></a>
