91ca1ad34ed80bc815bb7cb04bd41ad3ed59e8c5
Zach Moore <sluggermoore@gmail.com>
2023-05-11 12:59:41 -0400

    styling changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,29 +14,7 @@ export default function NavBar() {
     <nav>
     <ul>
       <li>
-        <a href="#about-me" onClick={handleLinkClick} className={activeLink === 'About Me' ? 'active' : ''}>
-          About Me
-        </a>
-      </li>
-      <li>
-        <a href="#skills" onClick={handleLinkClick} className={activeLink === 'Skills' ? 'active' : ''}>
-          Skills
-        </a>
-      </li>
-      <li>
-        <a href="#projects" onClick={handleLinkClick} className={activeLink === 'Projects' ? 'active' : ''}>
-          Projects
-        </a>
-      </li>
-      <li>
-        <a href="http://www.linkedin.com/in/zachmoore223" target="_blank" rel="noopener noreferrer">
-        <img id='github' src={linkedIn} alt="linkedIn logo" height="35px" width="35px"></img>
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/zachmoore223" target="_blank" rel="noopener noreferrer">
-          <img id='github' src={github} alt="github logo" height="35px" width="35px"></img>
-        </a>
+       <h3 className='name'>Zach Moore</h3> 
       </li>
       <li className='phone'>
        <p>419-612-7542</p> 
@@ -44,6 +22,19 @@ export default function NavBar() {
       <li className='email'>
       <a href="mailto:zachmoore223@gmail.com">zachmoore223@gmail.com</a>
       </li>
+      <li className='social-links'>
+      <a href="http://www.linkedin.com/in/zachmoore223" target="_blank" rel="noopener noreferrer"><p>LinkedIn</p></a>
+        <a href="http://www.linkedin.com/in/zachmoore223" target="_blank" rel="noopener noreferrer">
+        <img id='github' src={linkedIn} alt="linkedIn logo" height="35px" width="35px"></img>
+        </a>
+      </li>
+      <li className='social-links'>
+      <a href="https://github.com/zachmoore223" target="_blank" rel="noopener noreferrer"><p>GitHub</p></a>
+        <a href="https://github.com/zachmoore223" target="_blank" rel="noopener noreferrer">
+          <img id='github' src={github} alt="github logo" height="35px" width="35px"></img>
+        </a>
+      </li>
+
     </ul>
     </nav>
   );
