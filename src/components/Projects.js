14f10aa2ef8796c031b3e42f9012062ba2efdbb5
Zach Moore <sluggermoore@gmail.com>
2023-05-11 22:09:57 -0400

    buttons added to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,29 +7,49 @@ return (
         <li>
         
             <h4>HydroHomie</h4>
-            Technologies used: Java, React, Spring Boot, JSON, Javascript, HTML, CSS, REST API
             Designed and implemented a single page web design using React
             Enabled users to create a collection of plants from a self created plant API and then
             use that collection to create a watering schedule for their plants
+            <br/><br/>
+            <strong>Tech:</strong> Java, React, Spring Boot, JSON, Javascript, HTML, CSS, REST API
+            <br/><br/>
+            <a href="https://github.com/zachmoore223/PlantCalendar.git" target="_blank">
+            <button className="viewCodeButton"> View Code</button>
+            </a>
         </li>
 
         <li>
-            <h4>Virtual Pet API</h4>
-            Technologies used: Java, Spring Boot, JSON, Javascript, Thymeleaf, HTML, CSS, REST API
+            <h4>Virtual Pet API</h4>         
             Designed and implemented  a virtual pet application in a 3-person team over 2 weeks.
             Enabled users to create multiple virtual pet shelters which could hold multiple virtual pets and then perform actions such as “feed” on the pets.
+            <br/><br/>
+            <strong>Tech:</strong> Java, Spring Boot, JSON, Javascript, Thymeleaf, HTML, CSS, REST API
+            <br/><br/>
+            <a href="https://github.com/zachmoore223/PlantCalendar.git" target="_blank">
+            <button className="viewCodeButton"> View Code</button>
+            </a>
         </li>
         <li>       
-            <h4>Jac Enterprises </h4>
-            Technologies used: Java, React, Spring Boot, JSON, Javascript, HTML, CSS, REST API
+            <h4>Jac Enterprises </h4>    
             Designed and implemented a single page web design using React
-            Enabled users to interact with two external APIs interact with our own API server			
+            Enabled users to interact with two external APIs interact with our own API server
+            <br/><br/>
+            <strong>Tech:</strong> Java, React, Spring Boot, JSON, Javascript, HTML, CSS, REST API
+            <br/><br/>
+            <a href="https://github.com/zachmoore223/PlantCalendar.git" target="_blank">
+            <button className="viewCodeButton"> View Code</button>
+            </a>	
         </li> 
         <li>         
             <h4>Auto Format CSS</h4>
-            Technologies used: Java, Spring Boot, JSON, Javascript, Thymeleaf, HTML, CSS, REST API
             Designed and implemented  a CSS Formatter  over 2 weeks.
             Enabled users to select their own colors for various HTML tags and display the choices to the user
+            <br/><br/>
+            <strong>Tech:</strong> Java, Spring Boot, JSON, Javascript, Thymeleaf, HTML, CSS, REST API
+            <br/><br/>
+            <a href="https://github.com/zachmoore223/PlantCalendar.git" target="_blank">
+            <button className="viewCodeButton"> View Code</button>
+            </a>
           </li>   
         </ol>
       </div>
