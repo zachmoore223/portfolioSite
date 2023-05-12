b3a0047d4c1112ec67ceab4ecb9cd2a9d19f46e5
Zach Moore <sluggermoore@gmail.com>
2023-05-12 11:31:14 -0400

    responsive design

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ return (
 <div className="projects">
   <ol className="projectsList">
     
-        <li>
+        <li className="projectsLI">
         
             <h4>HydroHomie</h4>
             Designed and implemented a single page web design using React
@@ -13,41 +13,41 @@ return (
             <br/><br/>
             <strong>Tech:</strong> Java, React, Spring Boot, JSON, Javascript, HTML, CSS, REST API
             <br/><br/>
-            <a href="https://github.com/zachmoore223/PlantCalendar.git" target="_blank">
+            <a className='viewCodeLink' href="https://github.com/zachmoore223/PlantCalendar.git" target="_blank">
             <button className="viewCodeButton"> View Code</button>
             </a>
         </li>
 
-        <li>
+        <li className="projectsLI">
             <h4>Virtual Pet API</h4>         
             Designed and implemented  a virtual pet application in a 3-person team over 2 weeks.
             Enabled users to create multiple virtual pet shelters which could hold multiple virtual pets and then perform actions such as “feed” on the pets.
             <br/><br/>
             <strong>Tech:</strong> Java, Spring Boot, JSON, Javascript, Thymeleaf, HTML, CSS, REST API
             <br/><br/>
-            <a href="https://github.com/zachmoore223/PlantCalendar.git" target="_blank">
+            <a href="https://github.com/zachmoore223/VirtualPetAPI.git" target="_blank">
             <button className="viewCodeButton"> View Code</button>
             </a>
         </li>
-        <li>       
+        <li className="projectsLI">       
             <h4>Jac Enterprises </h4>    
             Designed and implemented a single page web design using React
             Enabled users to interact with two external APIs interact with our own API server
             <br/><br/>
             <strong>Tech:</strong> Java, React, Spring Boot, JSON, Javascript, HTML, CSS, REST API
             <br/><br/>
-            <a href="https://github.com/zachmoore223/PlantCalendar.git" target="_blank">
+            <a href="https://github.com/zachmoore223/JacEnterprises.git" target="_blank">
             <button className="viewCodeButton"> View Code</button>
             </a>	
         </li> 
-        <li>         
+        <li className="projectsLI">         
             <h4>Auto Format CSS</h4>
             Designed and implemented  a CSS Formatter  over 2 weeks.
             Enabled users to select their own colors for various HTML tags and display the choices to the user
             <br/><br/>
             <strong>Tech:</strong> Java, Spring Boot, JSON, Javascript, Thymeleaf, HTML, CSS, REST API
             <br/><br/>
-            <a href="https://github.com/zachmoore223/PlantCalendar.git" target="_blank">
+            <a href="https://github.com/zachmoore223/FormatCSS.git" target="_blank">
             <button className="viewCodeButton"> View Code</button>
             </a>
           </li>   
